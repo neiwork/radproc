7912dd7f3293b9e15cf6776e9af2c0cec35f6c19
neiwork <neiwork@users.noreply.github.com>
2016-06-10 18:19:02 +0200

    The non-thermal luminosity was higher than it should be; I fixed by evaluating at E=GeV, where the radiative losses are comparable with adiabatic. Now I obtained the same result as BR 2015

--- JetAGN/JetAGN/nonThermalLuminosity.cpp
+++ JetAGN/JetAGN/nonThermalLuminosity.cpp
@@ -16,7 +16,9 @@ double dLnt(double z)  //esta es la función que depende del número de estrellas 
 {
 	
 	//double Tstar = 3.0e3; 
-	double E = P2(electronMass*cLight2) / (boltzmann*parameters.starT);
+	double E = 1.0e9*1.6e-12;  //elijo 1GeV que es la E a la cual las perdidas rad son importantes
+	//con este valor reproduzco el 5x10^40 erg/s del paper de valenti
+	// electronMass*cLight2;// P2(electronMass*cLight2) / (boltzmann*parameters.starT);
 
 	double Sj = pi*P2(jetRadius(z, parameters.openingAngle));
 	double stagPoint = stagnationPoint(z);
@@ -26,11 +28,20 @@ double dLnt(double z)  //esta es la función que depende del número de estrellas 
 
 	double tad = adiabaticLosses(E, z, vel_lat) / E; //ad esta en erg/s
 
-	double wph = parameters.Lj / (cLight*4.0*Sj);
-	double trad = 4.0*cLight*thomson*wph*E / P2(electronMass*cLight2);
+	double wph = parameters.Lj *P2(pi) / (100.0*cLight*Sj);//  (cLight*4.0*Sj);
+	//double wph1 = parameters.Lj /  (cLight*4.0*Sj);
+
+	double wmag = P2(parameters.magneticField) / (8.0*pi);
+	double tsin = 4.0 * thomson*cLight*wmag*(E / P2(electronMass*cLight2)) / 3.0;
+	double tic = 4.0*cLight*thomson*wph*E / P2(electronMass*cLight2);
+	double trad = tsin + tic;
 
 	double frad = 1.0 / (1.0 + tad / trad);
 
+	if (So > Sj)
+	{
+		So = Sj;
+	}
 	double Lnt = parameters.accEfficiency*parameters.Lj*(So / Sj)*frad*pow(parameters.Dlorentz, 4) / P2(parameters.Gamma);
 
 	double f = starDensity(z)*Lnt; //saque el Sj
