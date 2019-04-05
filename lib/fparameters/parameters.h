8359ab03bcf09a2c068fa44051905f5ffebef9b4
neiwork <florenciavieyro@gmail.com>
2019-04-05 18:43:41 -0300

    fix dependencies on my computer

--- lib/fparameters/parameters.h
+++ lib/fparameters/parameters.h
@@ -1,5 +1,6 @@
 #pragma once
 
-#include <boost\property_tree\ptree_fwd.hpp>
+//#include <boost\property_tree\ptree_fwd.hpp>
+#include <boost\property_tree\ptree.hpp>
 
 extern boost::property_tree::ptree GlobalConfig;
